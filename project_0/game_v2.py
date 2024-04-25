b1b81c7ce07af3db30d35fa3465572d56a4b4f81
oeseo <ali301194@yandex.ru>
2024-04-25 20:54:51 +0500

    .

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,14 +1,24 @@
 import numpy as np
 
-def random_predict(number:int=1) -> int:
+def random_predict(number: int = 1) -> int:
+    """Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
     count = 0
 
     while True:
-        count+=1
-        predict_number = np.random.randint(1, 101) # предполагаемое число
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
         if number == predict_number:
-            break # выход из цикла, если угадали
-    return(count)
+            break  # выход из цикла если угадали
+    
+    return count
 
 
 def score_game(random_predict) -> int:
